fa599de45f94863303c78ea4645d476ca662d4fc
vsvrund <svrund2000@yahoo.com>
2023-11-29 13:55:14 +0000

    Latest Updates

--- MultinationalRetailDataCentralisation/ database_utils.py
+++ MultinationalRetailDataCentralisation/ database_utils.py
@@ -1,12 +1,63 @@
-class DatabaseConnector:
-    def __init__(self, db_name, username, password, host):
-        # Initialize database connection parameters
-        pass
+import yaml
+import sqlalchemy
+import psycopg2
+
+class DatabaseConnector():
+
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as file:
+            creds = yaml.safe_load(file)
+        return creds
     
-    def connect(self):
-        # Method to connect to the database
-        pass
-    
-    def upload_data(self, data):
-        # Method to upload data to the database
-        pass
+    def init_db_engine(self):
+        # Read the credentials from the YAML file
+        creds = self.read_db_creds()
+
+        # Extract the necessary credentials
+        database_type = 'postgresql'
+        host = creds['RDS_HOST']
+        username = creds['RDS_USER']
+        password = creds['RDS_PASSWORD']
+        database = creds['RDS_DATABASE']
+        port = creds['RDS_PORT']
+
+        # Construct the database connection URL
+        db_conn_url = f"{database_type}://{username}:{password}@{host}:{port}/{database}"
+
+        # Create and return the SQLAlchemy engine
+        engine = sqlalchemy.create_engine(db_conn_url)
+        return engine
+
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        with engine.connect() as connection:
+            # Get the metadata of the database
+            metadata = sqlalchemy.MetaData()
+            metadata.reflect(bind=connection)
+
+            # Get the table names from the metadata
+            table_names = metadata.tables.keys()
+
+            return table_names
+
+    def upload_to_db(self, df, table_name):
+
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = 'localhost'
+        USER = 'postgres'
+        PASSWORD = '....'
+        DATABASE = 'sales_data'
+        PORT = 5432
+        local_engine = sqlalchemy.create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        df.to_sql(table_name, local_engine, if_exists='replace')
+
+
+
+
+if __name__ == '__main__':
+    print('Tables in Database:', DatabaseConnector().list_db_tables())
+   
+    from data_cleaning import DataCleaning
+    from data_extraction import DataExtractor
+    DatabaseConnector().upload_to_db(DataCleaning().clean_user_data(),'dim_users')    
--- MultinationalRetailDataCentralisation/data_extraction.py
+++ MultinationalRetailDataCentralisation/data_extraction.py
@@ -1,12 +1,63 @@
-class DataExtractor:
-    def extract_from_csv(self, file_path):
-        # Method to extract data from CSV files
-        pass
+from database_utils import DatabaseConnector
+import sqlalchemy
+import pandas as pd
+import requests
+import tabula
+import boto3 
+
+class DataExtractor():
+
+    def __init__(self):
+        self.db = DatabaseConnector()
+        self.rds_database = self.db.init_db_engine()
+        self.api_key ={'x-api-key':'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+
+
+    def read_rds_table(self, table_name):
+      return pd.read_sql_table(table_name, self.rds_database)
     
-    def extract_from_api(self, api_endpoint):
-        # Method to extract data from an API
-        pass
+
+    def retrieve_pdf_data(self, pdf_link):
+        pdf_dataframes = tabula.read_pdf(pdf_link, pages="all", multiple_tables=True)
+
+        return pd.concat(pdf_dataframes,ignore_index=True)
     
-    def extract_from_s3(self, bucket_name, object_key):
-        # Method to extract data from an S3 bucket
-        pass
+
+    def list_number_of_store(self):
+        stores = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores',headers=self.api_key)
+        number_of_stores = stores.json()
+        return number_of_stores["number_stores"]
+    
+
+    def retrieve_stores_data(self):
+        number_of_stores = self.list_number_of_store()
+        stores_list = []
+        for store_number in range(0, number_of_stores):
+            stores_list.append(requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'+str(store_number), headers=self.api_key).json())
+        return pd.json_normalize(stores_list)
+    
+
+    def extract_from_s3(self,address):
+        s3 = boto3.client('s3')
+        bucket = 'data-handling-public'
+        object = 'products.csv'
+        file = 'products.csv'
+        s3.download_file(bucket,object,file)
+        table = pd.read_csv('./products.csv')
+        return table
+    
+
+    def extract_from_s3_json(self):
+        #https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json
+        s3 = boto3.client('s3')
+        bucket = 'data-handling-public'
+        object = 'date_details.json'
+        file = 'date_details.json'
+        s3.download_file(bucket,object,file)
+        table = pd.read_json('./date_details.json')
+        # print(table)
+        return table
+    
+
+if __name__ == "__main__":
+    isinstance = DataExtractor()
